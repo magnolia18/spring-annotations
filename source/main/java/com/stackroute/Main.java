d93a185f773dd1bffe14dc0d26721865c398790b
Magnolia JR <magnoliajr18@gmail.com>
2019-07-15 10:41:24 +0530

    Update Main.java

--- source/main/java/com/stackroute/Main.java
+++ source/main/java/com/stackroute/Main.java
@@ -1,52 +1,39 @@
+
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.*;
 import org.springframework.beans.factory.xml.ResourceEntityResolver;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
-import java.sql.SQLOutput;
-
 public class Main {
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.stackroute.domain");
 
-    public static void main(String[]args) {
-
-//using application context
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-//scope=singleton
-        Movie movie1 = context.getBean("movie1", Movie.class);
+        Movie movie1 = context.getBean("movieObj1",Movie.class);
         movie1.display();
 
-        Movie movieB = context.getBean("movie1", Movie.class);
-        movie1.display();
-
-        Movie movie2 = context.getBean("movie2", Movie.class);
+        Movie movie2 = context.getBean("movieObj2",Movie.class);
         movie2.display();
 
-        Movie movie3 = context.getBean("movie3", Movie.class);
+        Movie movie3 = context.getBean("movieObj3",Movie.class);
         movie3.display();
 
-//scope= prototype
-        Movie movie4 = context.getBean("movie4", Movie.class);
-        movie4.display();
+        Movie movie4 = context.getBean("movieObj3",Movie.class);
 
-        Movie movie5 = context.getBean("movie4", Movie.class);
-        movie5.display();    //gives null
+        System.out.println(movie3 == movie4);
 
-        System.out.println(movie1 == movieB);
-
-
+        Movie movie5 = (Movie) context.getBean("movieObject");
+        movie5.display();
     }
 
 }
